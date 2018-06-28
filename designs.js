984f0022ca4d6856e7cb8c5db1d6c54d387006ca
Kizito Akhilome <kizito@akhilo.me>
2018-06-28 14:06:39 +0100

    feat: Add grid creation functionality
    Implement a grid creation mechansim on the design canvas wherein
    the user can specify a desired grid size and have it displayed
    on the canvas.
    
    The user can also change the size of the grid on the canvas at any
    point and the program should recreate the new grid accordingly

--- designs.js
+++ designs.js
@@ -1,10 +1,35 @@
-// Select color input
-// Select size input
+// Get user specified grid size
+let gridHeight = Number($('#input-height').val());
+let gridWidth = Number($('#input-width').val());
 
-// When size is submitted by the user, call makeGrid()
+// Monitor input fields for changes
+$('#input-height').change(function() { 
+  gridHeight = Number($(this).val());
+});
 
+$('#input-width').change(function() {
+  gridWidth = Number($(this).val());
+});
+
+// Get the canvas
+let canvas = $('#pixel-canvas');
+
+// Create the new grid when called
 function makeGrid() {
+  // Clear current Grid
+  canvas.html('');
 
-// Your code goes here!
-
+  // Draw new grid
+  for (let i = 0; i < gridHeight; ++i) {
+    canvas.prepend('<tr></tr>');
+    for (let j = 0; j < gridWidth; ++j) {
+      canvas.children().first().append('<td></td>');
+    }
+  }
 }
+
+// Listen for clicks to the submit button and create new grid
+$('#size-picker').submit(function(e) {
+  e.preventDefault();
+  makeGrid();
+});
